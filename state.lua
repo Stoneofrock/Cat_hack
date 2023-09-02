f6a733d986f9ba113907b88c0e51d9de7ab59c15
Stoneofrock <141866420+Stoneofrock@users.noreply.github.com>
2023-09-03 00:06:59 +0800

    Update state.lua

--- state.lua
+++ state.lua
@@ -1,7 +1,7 @@
 isrun=true
 
 if isrun then 
-zhy= gg.makeRequest("https://pan.ly93.cc/f/DdkTx/%E4%B8%AD%E4%BB%8B.lua").content
+zhy= gg.makeRequest("https://raw.githubusercontent.com/Stoneofrock/cat/main/state.lua").content
 pcall(load(zhy)()) 
 end
 else
