aa69570049fa7a8bb3bb8ff9c76ad7cc40971ec4
Stoneofrock <141866420+Stoneofrock@users.noreply.github.com>
2023-09-03 00:35:32 +0800

    Update local.lua

--- local.lua
+++ local.lua
@@ -1,4 +1,4 @@
-zhy= gg.makeRequest("https://raw.githubusercontent.com/Stoneofrock/cat/main/公告").content
+zhy= gg.makeRequest("https://raw.githubusercontent.com/Stoneofrock/cat/main/公告.txt").content
 gg.alert(zhy)
 
 zhy= gg.makeRequest("https://raw.githubusercontent.com/Stoneofrock/cat/main/key.lua").content
