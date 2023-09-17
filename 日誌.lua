5459a7b863611b03c654640f375976eb354f1130
Stoneofrock <141866420+Stoneofrock@users.noreply.github.com>
2023-09-17 20:45:46 +0800

    Create 日誌.lua

---
+++ 日誌.lua
@@ -0,0 +1,41 @@
+local http = require("socket.http")
+local ltn12 = require("ltn12")
+
+local username = "Stoneofrock"
+local token = "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" -- 訪問令牌 (Personal Access Token)
+local repoOwner = "RepositoryOwnerUsername"
+local repoName = "RepositoryName"
+local filePath = "number" -- 要上傳的文件的路徑
+local branch = "main" -- 分支名稱
+
+local apiUrl = string.format("https://api.github.com/repos/%s/%s/contents/%s", repoOwner, repoName, filePath)
+
+local fileContent = io.open(filePath, "rb"):read("*a")
+local base64Content = mime.b64(fileContent)
+
+local requestBody = {
+  message = "Add file via Lua script",
+  content = base64Content,
+  branch = branch
+}
+
+local headers = {
+  ["Authorization"] = "token " .. token,
+  ["User-Agent"] = username
+}
+
+local request = {
+  url = apiUrl,
+  method = "PUT",
+  headers = headers,
+  source = ltn12.source.string(require("json").encode(requestBody))
+}
+
+local response = {}
+local _, statusCode, _, _ = http.request(request, response)
+
+if statusCode == 201 then
+  print("File uploaded successfully.")
+else
+  print("Error uploading file. Status code: " .. statusCode)
+end
