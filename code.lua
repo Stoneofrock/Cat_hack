e231f0cea9eaca9c4c34422182f17ee893e8d84f
Stoneofrock <141866420+Stoneofrock@users.noreply.github.com>
2023-09-17 20:47:31 +0800

    Update code.lua

--- code.lua
+++ code.lua
@@ -67,7 +67,7 @@ function p()
   function reset()
   gg.alert("重設完成")
   text= gg.makeRequest("https://pan.ly93.cc/f/Nl0HB/reset.lua").content
-  file = io.open("網路連接腳本.lua", "w") 
+  file = io.open("local.lua", "w") 
   file:write(text)
   file:close()
   os.exit()
