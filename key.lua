36d116078b2cdb81df95c4c503cabc7710854a1b
Stoneofrock <141866420+Stoneofrock@users.noreply.github.com>
2023-12-02 22:52:53 +0800

    Update key.lua

--- key.lua
+++ key.lua
@@ -7,7 +7,7 @@ local cards = {
     { cardname = "32", password = "948788822", isuse = false, cardtype = "32wifi" },
     { cardname = "64", password = "948788822", isuse = true, cardtype = "64wifi" },
     { cardname = "32", password = "root32", isuse = false, cardtype = "32wifi" },
-    { cardname = "test", password = "test_key", isuse = false, cardtype = "32wifi" },
+    { cardname = "test", password = "test_key", isuse = true, cardtype = "32wifi" },
     -- ... 您可以按需要添加更多的卡蜜信息
 }
 
