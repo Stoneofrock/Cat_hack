9dec3b137abaf77fc4ba76d88b7b7bdf27798424
Stoneofrock <141866420+Stoneofrock@users.noreply.github.com>
2024-02-17 11:56:09 +0800

    Update update.lua

--- update.lua
+++ update.lua
@@ -45,9 +45,10 @@ end
 
 if localContent == onlineContent then
     gg.alert("目前為最新版本")
-     writeFile2("log.txt", 0)
+    
     zhy = gg.makeRequest("https://raw.githubusercontent.com/Stoneofrock/cat/main/code.lua").content
     pcall(load(zhy)()) 
+     writeFile2("log.txt", 0)
 else
     gg.alert("已有最新版本")
     local targetFilePath = "key.lua"
